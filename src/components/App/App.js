b53cb1e1f32dcf3bf2f4ef1bd50d8739aff88fd6
tishkov <tishkov834@gmail.com>
2022-02-11 10:19:13 +0200

    Create RegistrationPage component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,11 @@
+import RegistrationPage from '../../pages/RegistrationPage';
 import './styles.scss';
 
 function App() {
   return (
-    <div id="wrapper" />
+    <div id="wrapper">
+      <RegistrationPage />
+    </div>
   );
 }
 
