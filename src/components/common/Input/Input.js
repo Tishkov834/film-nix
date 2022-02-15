13be15d329b9e85d66f83602b07def69bbe23e76
tishkov <tishkov834@gmail.com>
2022-02-15 09:14:51 +0200

    Add props to Input component

--- src/components/common/Input/Input.js
+++ src/components/common/Input/Input.js
@@ -2,18 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-function Input({ placeholderText }) {
+function Input({
+  type, value, onChange, placeholderText,
+}) {
   return (
-    <input className="input" type="text" placeholder={placeholderText} />
+    <input className="input" type={type} value={value} onChange={onChange} placeholder={placeholderText} />
   );
 }
 
 Input.propTypes = {
   placeholderText: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 Input.defaultProps = {
   placeholderText: '',
+  type: '',
+  value: '',
+  onChange: () => {},
 };
 
 export default Input;
