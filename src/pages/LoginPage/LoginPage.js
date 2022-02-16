8e057ab723b74cb6085c81ab7325e1f13e820cc6
tishkov <tishkov834@gmail.com>
2022-02-16 10:33:48 +0200

    Create LoginPage component

---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import AuthForm from '../../components/AuthForm';
+import Input from '../../components/common/Input';
+import Button from '../../components/common/Button';
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <AuthForm heading="Login" text="Don't have an account?" textLink="Sing up here">
+      <Input
+        placeholderText="Email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholderText="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit">Login</Button>
+    </AuthForm>
+  );
+}
+
+export default LoginPage;
