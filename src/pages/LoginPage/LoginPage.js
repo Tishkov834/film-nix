727badb35290412af723ae19ab661972f9bb7197
tishkov <tishkov834@gmail.com>
2022-02-16 11:48:22 +0200

    Add logic to LoginPage compoment

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,15 +1,39 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import AuthForm from '../../components/AuthForm';
 import Input from '../../components/common/Input';
 import Button from '../../components/common/Button';
-import { REGISTRATION } from '../../constants/routes';
+import { HOME_PAGE, REGISTRATION } from '../../constants/routes';
+import { login } from '../../api/users';
+import { setUser } from '../../store/authorization/authReducer';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    login({
+      email,
+      password,
+    })
+      .then(({ data }) => {
+        const { accessToken, user } = data;
+        dispatch(setUser({ token: accessToken, user }));
+        navigate(HOME_PAGE);
+      })
+      .finally(() => setIsLoading(false));
+  };
 
   return (
-    <AuthForm heading="Login" text="Don't have an account?" textLink="Sing up here" link={REGISTRATION}>
+    <AuthForm heading="Login" text="Don't have an account?" textLink="Sing up here" link={REGISTRATION} onSubmit={handleLogin}>
       <Input
         placeholderText="Email"
         type="email"
@@ -22,7 +46,7 @@ function LoginPage() {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <Button type="submit">Login</Button>
+      <Button type="submit" isDisabled={isLoading}>Login</Button>
     </AuthForm>
   );
 }
