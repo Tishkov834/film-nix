aaa9f539955cce1bf61fb0c2256c1854267e3878
tishkov <tishkov834@gmail.com>
2022-02-17 13:49:02 +0200

    Add logout logic to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,40 @@
-import { Link } from 'react-router-dom';
-import { HOME_PAGE, LOGIN } from '../../constants/routes';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { HOME_PAGE, LOGIN, REGISTRATION } from '../../constants/routes';
+import { removeUser } from '../../store/authorization/authReducer';
 import Logo from '../../images/logo.svg';
 import Login from '../../images/icons/login.svg';
+import Logout from '../../images/icons/logout.svg';
 import './styles.scss';
 
 function Header() {
+  const isUserAuthorized = useSelector(({ authorization }) => authorization.token);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const handleLogout = () => {
+    dispatch(removeUser());
+    navigate(LOGIN);
+  };
+
   return (
     <div className="header">
       <Link to={HOME_PAGE} className="header-logo">
         <img src={Logo} alt="logo" className="header-logo-image" />
         <p className="header-text">Film&NIX</p>
       </Link>
-      <Link to={LOGIN} className="header-link">
-        <p className="header-text">Login</p>
-        <img src={Login} alt="login" className="header-link-image" />
-      </Link>
+      {isUserAuthorized ? (
+        <button className="header-button" onClick={handleLogout}>
+          <p className="header-text">Logout</p>
+          <img src={Logout} alt="login" className="header-link-image" />
+        </button>
+      ) : (
+        <Link to={(pathname === LOGIN) ? REGISTRATION : LOGIN} className="header-link">
+          <p className="header-text">{(pathname === LOGIN) ? 'Registration' : 'Login'}</p>
+          <img src={Login} alt="login" className="header-link-image" />
+        </Link>
+      )}
     </div>
   );
 }
