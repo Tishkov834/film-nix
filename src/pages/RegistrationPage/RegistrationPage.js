7bcefe24c91b0c74846a47c92d11ba89d3c7a5da
tishkov <tishkov834@gmail.com>
2022-02-21 10:53:36 +0200

    Add validation logic to RegistrationPage component

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -7,27 +7,28 @@ import Button from '../../components/common/Button';
 import { register } from '../../api/users';
 import { setUser } from '../../store/authorization/authReducer';
 import { HOME_PAGE, LOGIN } from '../../constants/routes';
+import { registrationSchema } from '../../schemas/authSchema';
 
 function RegistrationPage() {
-  const [username, setUsername] = useState('');
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleRegister = (event) => {
-    event.preventDefault();
+  const initialValues = {
+    username: '',
+    name: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  };
 
+  const handleRegister = ({
+    username, name, email, password,
+  }) => {
     setIsLoading(true);
 
     register({
-      username,
-      name,
-      email,
-      password,
+      username, name, email, password,
     })
       .then(({ data }) => {
         const { accessToken, user } = data;
@@ -38,37 +39,20 @@ function RegistrationPage() {
   };
 
   return (
-    <AuthForm heading="Registration" text="Already have an account?" textLink="Login here" link={LOGIN} onSubmit={handleRegister}>
-      <Input
-        placeholderText="Login"
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <Input
-        placeholderText="Name"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Input
-        placeholderText="Email"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Input
-        placeholderText="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Input
-        placeholderText="Repeat password"
-        type="password"
-        value={repeatPassword}
-        onChange={(e) => setRepeatPassword(e.target.value)}
-      />
+    <AuthForm
+      heading="Registration"
+      text="Already have an account?"
+      textLink="Login here"
+      link={LOGIN}
+      onSubmit={handleRegister}
+      initialValues={initialValues}
+      validationSchema={registrationSchema}
+    >
+      <Input type="text" name="username" placeholderText="Login" />
+      <Input type="text" name="name" placeholderText="Name" />
+      <Input type="email" name="email" placeholderText="Email" />
+      <Input type="password" name="password" placeholderText="Password" />
+      <Input type="password" name="repeatPassword" placeholderText="Repeat password" />
       <Button type="submit" isDisabled={isLoading}>Registration</Button>
     </AuthForm>
   );
