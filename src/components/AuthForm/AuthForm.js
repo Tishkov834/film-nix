6e901e3f4fe9d45b69733c1aed0e414290e52a43
tishkov <tishkov834@gmail.com>
2022-02-21 10:51:08 +0200

    Add formik to AuthForm component

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Formik } from 'formik';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
 function AuthForm({
-  heading, children, text, textLink, link, onSubmit,
+  heading, children, text, textLink, link, onSubmit, initialValues, validationSchema,
 }) {
   return (
     <div className="auth-form">
       <h1 className="auth-form-heading">{heading}</h1>
-      <form className="auth-form-content" onSubmit={onSubmit}>{children}</form>
+      <Formik onSubmit={onSubmit} initialValues={initialValues} validationSchema={validationSchema}>
+        <Form className="auth-form-content">{children}</Form>
+      </Formik>
       <div className="auth-form-redirect">
         <p className="auth-form-redirect-text">{text}</p>
         <Link className="auth-form-redirect-link" to={link}>{textLink}</Link>
@@ -25,6 +28,8 @@ AuthForm.propTypes = {
   link: PropTypes.string,
   children: PropTypes.element,
   onSubmit: PropTypes.func,
+  initialValues: PropTypes.instanceOf(Object),
+  validationSchema: PropTypes.instanceOf(Object),
 };
 
 AuthForm.defaultProps = {
@@ -34,6 +39,8 @@ AuthForm.defaultProps = {
   link: '',
   children: '',
   onSubmit: () => {},
+  initialValues: {},
+  validationSchema: {},
 };
 
 export default AuthForm;
