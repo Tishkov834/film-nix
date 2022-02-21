c6263b0e8cc2d64da9c08a820808d6688f275799
tishkov <tishkov834@gmail.com>
2022-02-21 12:00:55 +0200

    Add error checking to LoginPage component

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useAlert } from 'react-alert';
 import AuthForm from '../../components/AuthForm';
 import Button from '../../components/common/Button';
 import Input from '../../components/common/Input';
@@ -13,6 +14,7 @@ function LoginPage() {
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const alert = useAlert();
 
   const initialValues = {
     email: '',
@@ -27,6 +29,7 @@ function LoginPage() {
       dispatch(setUser({ token: accessToken, user }));
       navigate(HOME_PAGE);
     })
+      .catch(() => alert.error('Incorrect email or password!'))
       .finally(() => setIsLoading(false));
   };
 
