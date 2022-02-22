7dad258ac25fd4648d892ad5074a859c59b9cca9
tishkov <tishkov834@gmail.com>
2022-02-22 11:36:26 +0200

    Add CreateFilmModal component to UserPage

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import Button from '../../components/common/Button';
+import CreateFilmModal from '../../components/CreateFilmModal';
 
 function UserPage() {
+  const [isCreateFilmModalOpen, setCreateFilmModalOpen] = useState(false);
+
   return (
-    <>
+    <div>
       <h1>User page</h1>
-      <Button>Add Film</Button>
-    </>
+      <Button type="button" onClick={() => setCreateFilmModalOpen(true)}>Add Film</Button>
+      <CreateFilmModal active={isCreateFilmModalOpen} closeModal={setCreateFilmModalOpen} />
+    </div>
   );
 }
 
