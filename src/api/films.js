4574107dfb54757602d1344f8c0fa3887d5472a3
Dmytro Tishkov <tishkov834@gmail.com>
2022-03-04 18:32:52 +0200

    Add getFilm api call

--- src/api/films.js
+++ src/api/films.js
@@ -4,3 +4,5 @@ import { FILMS_URL } from '../constants/endpoints';
 export const createFilm = (filmData) => axiosInstance.post(FILMS_URL, filmData);
 
 export const getAllFilms = () => axiosInstance.get(FILMS_URL);
+
+export const getFilm = (id) => axiosInstance.get(`${FILMS_URL}/${id}`);
