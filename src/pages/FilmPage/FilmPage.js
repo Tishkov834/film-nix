8528b7ed0f02ef91203d16ef7029d3d8b20abe88
Dmytro Tishkov <tishkov834@gmail.com>
2022-03-06 15:33:29 +0200

    Add FilmDetailCard to FilmPage component

--- src/pages/FilmPage/FilmPage.js
+++ src/pages/FilmPage/FilmPage.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getFilm } from '../../api/films';
+import FilmDetailCard from '../../components/FilmDetailCard';
 
 function FilmPage() {
-  const [film, setFilm] = useState({});
+  const [film, setFilm] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
@@ -12,7 +13,7 @@ function FilmPage() {
       .catch(({ message }) => alert.error(message));
   }, []);
 
-  return <h1>{film.name}</h1>;
+  return (film && <FilmDetailCard film={film} />);
 }
 
 export default FilmPage;
