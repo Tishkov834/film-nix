45c842eec294f3342ced5945a8c7af3c81811b60
Dmytro Tishkov <tishkov834@gmail.com>
2022-03-09 19:26:06 +0200

    Add getAllFilms api call

--- src/api/films.js
+++ src/api/films.js
@@ -2,3 +2,5 @@ import axiosInstance from './axiosInstance';
 import { FILMS_URL } from '../constants/endpoints';
 
 export const createFilm = (filmData) => axiosInstance.post(FILMS_URL, filmData);
+
+export const getAllFilms = () => axiosInstance.get(FILMS_URL);
