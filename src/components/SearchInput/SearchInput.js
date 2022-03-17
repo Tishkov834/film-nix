e738f14cfcc9322309e483652893d934c0bb1aed
Dmytro Tishkov <tishkov834@gmail.com>
2022-03-17 20:22:25 +0200

    Add props to SearchInput component

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -2,21 +2,25 @@ import PropTypes from 'prop-types';
 import Search from '../../images/icons/search-icon.svg';
 import './styles.scss';
 
-function SearchInput({ onSubmit }) {
+function SearchInput({ onSubmit, value, onChange }) {
   return (
     <form className="search-form" onSubmit={onSubmit}>
       <img src={Search} alt="search" className="search-form-image" />
-      <input className="search-form-input" type="text" placeholder="Search Movies" />
+      <input className="search-form-input" type="text" placeholder="Search Movies" value={value} onChange={onChange} />
     </form>
   );
 }
 
 SearchInput.propTypes = {
   onSubmit: PropTypes.func,
+  onChange: PropTypes.func,
+  value: PropTypes.string,
 };
 
 SearchInput.defaultProps = {
   onSubmit: () => {},
+  onChange: () => {},
+  value: '',
 };
 
 export default SearchInput;
