c2f2dd15109f742bb6808ca8a3b123228d616ebb
Dmytro Tishkov <tishkov834@gmail.com>
2022-03-22 18:50:25 +0200

    Add loader to UserPage component

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -3,18 +3,25 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import FilmsList from '../../components/FilmsList';
 import Layout from '../../components/Layout';
+import Loader from '../../components/Loader';
 import { ADD_FILM_PAGE } from '../../constants/routes';
 import { getUserFilms } from '../../api/films';
 import './styles.scss';
 
 function UserPage() {
   const [films, setFilms] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const { id, name } = useSelector(({ authorization }) => authorization.user);
 
   useEffect(() => {
+    setIsLoading(true);
+
     getUserFilms(id)
       .then(({ data }) => setFilms(data))
-      .catch(({ message }) => alert.error(message));
+      .catch(({ message }) => alert.error(message))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   const onFilmDelete = (filmId) => {
@@ -28,6 +35,7 @@ function UserPage() {
         <Link to={ADD_FILM_PAGE} className="user-page-link">Add Film</Link>
       </div>
       <FilmsList films={films} onFilmDelete={onFilmDelete} />
+      {isLoading && <Loader />}
     </Layout>
   );
 }
