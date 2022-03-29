944db95c04d79f0effe4677455a11c3f15812664
Dmytro Tishkov <tishkov834@gmail.com>
2022-03-29 15:49:57 +0300

    Add starsCount to Rating component

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import { FaStar } from 'react-icons/fa';
 import './styles.scss';
 
-function Rating({ handleStarClick, rating }) {
+function Rating({ handleStarClick, rating, starsCount }) {
   const rows = [];
-  for (let i = 0; i <= 4; i += 1) {
+  for (let i = 0; i < starsCount; i += 1) {
     rows.push(<FaStar onClick={() => handleStarClick(i + 1)} className={i <= rating - 1 ? 'rating-star-click' : 'rating-star'} />);
   }
 
@@ -18,11 +18,13 @@ function Rating({ handleStarClick, rating }) {
 Rating.propTypes = {
   handleStarClick: PropTypes.func,
   rating: PropTypes.number,
+  starsCount: PropTypes.number,
 };
 
 Rating.defaultProps = {
   handleStarClick: () => {},
   rating: 0,
+  starsCount: 5,
 };
 
 export default Rating;
